e11b864b6cc6c786dc86ff368a86df4592c294b4
Brendson <brendson.net@gmail.com>
2022-10-02 09:27:33 -0300

    feat: criando component de loading

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,8 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import loadingModule from './loading';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modules: {
+        loading: loadingModule,
+    },
+});
