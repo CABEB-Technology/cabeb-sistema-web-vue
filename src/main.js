f9fb3f6ab8af4dbd17858c4617484b255b90ff8a
Brendson <brendson.net@gmail.com>
2022-10-01 23:00:37 -0300

    chore: configurando main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(PrimeVue)
+app.use(ToastService)
+
+// components prime
+
+
+app.mount('#app')
